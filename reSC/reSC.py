b47ef94c9980188fe6990c7696c5ebd48bcae80b
underflow101 <ikarus125@gmail.com>
2020-02-17 09:32:22 +0900

    update
    Signed-off-by: underflow101 <ikarus125@gmail.com>

---
+++ reSC/reSC.py
@@ -0,0 +1,20 @@
+# Regular Expression with Spark
+
+import re
+from pyspark import SparkContext, SparkConf, RDD
+
+conf = SparkConf().setAppName('Word Counts')
+sc = SparkContext(conf = conf)
+
+doc = sc.textFile("file:///opt/spark/data/shakespeare.txt")
+
+flattened = doc.filter(lambda line: len(line) > 0).flatMap(lambda line: re.split('\W+', line))
+flattened.take(6)
+
+kvpairs = flattened.filter(lambda word: len(word) > 0).map(lambda word:(word.lower(), 1))
+kvpairs.take(5)
+countsbyword = kvpairs.reduceByKey(lambda v1, v2: v1 + v2).sortByKey(ascending=False)
+countsbyword.take(5)
+
+topwords = countsbyword.map(lambda x: (x[1], x[0])).sortByKey(ascending=False)
+topwords.take(5)
